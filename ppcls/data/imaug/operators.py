c942a7ee728a1e6666960005261a729ec36a9587
will-jl944 <jiafeng.lu@duke.edu>
2021-03-19 16:47:52 +0800

    0

--- ppcls/data/imaug/operators.py
+++ ppcls/data/imaug/operators.py
@@ -228,7 +228,7 @@ class NormalizeImage(object):
                           np.ndarray), "invalid input 'img' in NormalizeImage"
         img = (img.astype('float32') * self.scale - self.mean) / self.std
         # print("Normalize image: {} s".format(time.time() - tic))
-        print('after normalize: ', np.mean(img))
+        # print('after normalize: ', np.mean(img))
         return img
 
 
